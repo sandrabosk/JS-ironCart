8099924217e25482c74ec279a6c138502c081387
sandrabosk <sandraboskovic@hotmail.com>
2018-04-11 08:14:51 -0400

    setting the code in right oreder

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -36,7 +36,7 @@ function getPriceByProduct(){
 // all total individual prices (after we multiply price of single product and quantity that user inserted)
 updatedPricesArray = document.getElementsByClassName('total-individual-price');
 
-//Add Total Product Prices and Update Total Price
+// just regular function for adding any two numbers
 function addTotalProductPrice(a, b) {
   return a + b;
 }
@@ -54,54 +54,19 @@ document.getElementById('theTotal').innerHTML = total
 
 }
 
-// 1st
-function createQuantityInput(){
-  // create quantity input field
-  var quantityInputField = document.createElement("input");
-  quantityInputField.setAttribute("type", "text");
-  quantityInputField.setAttribute("class", "quantity-input");
-  quantityInputField.setAttribute("placeholder", "0");
 
-  // return quantity input
-  return quantityInputField;
+// 1st => create name
+function createNameNode(newName){
+  var nameDiv = document.createElement("div");
+  nameDiv.setAttribute("class", "col-lg-2")
+  var nameSpan = document.createElement("span");
+  nameSpan.setAttribute("class", "product-name");
+  nameSpan.innerHTML = newName;
+  nameDiv.appendChild(nameSpan)
+  return nameDiv;
 }
 
-// function createDeleteButton(){
-
-// }
-
-function createIndividualPrice(){
-  var priceDiv = document.createElement('div');
-  priceDiv.setAttribute("class", "col-lg-2")
-  var dollarSignSpan = document.createElement("span");
-  dollarSignSpan.setAttribute("class", "dollar-sign");
-  dollarSignSpan.innerHTML = "$";
-  var totalIndPrice = document.createElement("span");
-  totalIndPrice.setAttribute("class", "total-individual-price");
-  totalIndPrice.innerHTML = "0";
-console.log("nandan")
-  // insert spans on DOM
-  priceDiv.appendChild(dollarSignSpan);
-  priceDiv.appendChild(totalIndPrice);
-  return priceDiv;
-}
-
-// 2nd
-function createQuantityNode(){
-  var quantityNode = document.createElement('div');
-  quantityNode.setAttribute("class", "col-lg-4")
-  var label = document.createElement("label");
-  label.innerHTML = "QTY";
-
-  var quantity = createQuantityInput();
-
-  quantityNode.appendChild(label);
-  quantityNode.appendChild(quantity);
-
-  // console.log("quantity node is: ", quantityNode)
-  return quantityNode
-}
-
+// 2nd => create price
 function createSinglePriceNode(newPrice){
   var singlePriceDiv = document.createElement('div');
   singlePriceDiv.setAttribute("class", "col-lg-2")
@@ -119,32 +84,68 @@ function createSinglePriceNode(newPrice){
 
   return singlePriceDiv;
 }
-// function createItemNode(dataType, itemData){
+
+// 3rd => create quantity input
+function createQuantityInput(){
+  // create quantity input field
+  var quantityInputField = document.createElement("input");
+  quantityInputField.setAttribute("type", "text");
+  quantityInputField.setAttribute("class", "quantity-input");
+  quantityInputField.setAttribute("placeholder", "0");
+
+  // return quantity input
+  return quantityInputField;
+}
+
+// 4th => create the whole quantity node
+function createQuantityNode(){
+  var quantityNode = document.createElement('div');
+  quantityNode.setAttribute("class", "col-lg-4")
+  var label = document.createElement("label");
+  label.innerHTML = "QTY";
+
+  var quantity = createQuantityInput();
+
+  quantityNode.appendChild(label);
+  quantityNode.appendChild(quantity);
+
+  // console.log("quantity node is: ", quantityNode)
+  return quantityNode
+}
+
+// 5th => create the individual total price
+function createIndividualPrice(){
+  var priceDiv = document.createElement('div');
+  priceDiv.setAttribute("class", "col-lg-2")
+  var dollarSignSpan = document.createElement("span");
+  dollarSignSpan.setAttribute("class", "dollar-sign");
+  dollarSignSpan.innerHTML = "$";
+  var totalIndPrice = document.createElement("span");
+  totalIndPrice.setAttribute("class", "total-individual-price");
+  totalIndPrice.innerHTML = "0";
+
+  // insert spans on DOM
+  priceDiv.appendChild(dollarSignSpan);
+  priceDiv.appendChild(totalIndPrice);
+  return priceDiv;
+}
+
+// 6th => create delete button
+// function createDeleteButton(){
 
 // }
 
-// create name
-function createNameNode(newName){
-  var nameDiv = document.createElement("div");
-  nameDiv.setAttribute("class", "col-lg-2")
-  var nameSpan = document.createElement("span");
-  nameSpan.setAttribute("class", "product-name");
-  nameSpan.innerHTML = newName;
-  nameDiv.appendChild(nameSpan)
-  return nameDiv;
-}
-
-// 3rd
+// 7th => creates the whole row
 function createNewItemRow(itemName, itemPrice){
   var productRow = document.createElement('div');
   var productName = createNameNode(itemName);
   var productPrice = createSinglePriceNode(itemPrice);
   var quantityNode = createQuantityNode();
   var indvidualTotalPrice = createIndividualPrice();
-// console.log("quantityNode is: ====", quantityNode)
-productRow.append(productName);
-productRow.append(productPrice);
 
+  // console.log("quantityNode is: ====", quantityNode)
+  productRow.append(productName);
+  productRow.append(productPrice);
   productRow.append(quantityNode);
   productRow.append(indvidualTotalPrice);
 
@@ -152,15 +153,15 @@ productRow.append(productPrice);
   return productRow;
 } 
 
-// 4th
+// 8th => sets the row on the DOM, positioning it above input fields to create new products
 function createNewItem(){
   var newProductName = document.getElementById('new-product-name').value;
   var newProductPrice = document.getElementById('new-product-price').value;
 
   var newItem = createNewItemRow(newProductName, newProductPrice);
-  console.log("new item is: ", newItem)
+  // console.log("new item is: ", newItem)
   var newDiv = document.getElementById('create');
-  console.log("new div is: ", newDiv);
+  // console.log("new div is: ", newDiv);
 
   newDiv.parentNode.insertBefore(newItem, newDiv)
 
@@ -168,6 +169,7 @@ function createNewItem(){
   document.getElementById('new-product-price').value = "";
 }
 
+// runs all the times
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
