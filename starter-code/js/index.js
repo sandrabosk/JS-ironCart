30ddc78e55d2d7bcb949d594da3c724a7be63f7a
sandrabosk <sandraboskovic@hotmail.com>
2018-04-11 17:52:55 -0400

    adding delete button and the final notes

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -9,6 +9,7 @@ function deleteItem(e){
   getTotalPrice();
 }
 
+// help us to limit decimals to 2
 function toTwoDecimals(rawValue){
   var twoDecimalsValue = parseFloat(Math.round(rawValue * 100)/100).toFixed(2);
   return twoDecimalsValue;
@@ -16,6 +17,7 @@ function toTwoDecimals(rawValue){
 
 // all the prices
 var productPrices = document.getElementsByClassName('single-item-price');
+console.log("blah: ====", productPrices)
 
 // all the products
 var allTheProducts = document.getElementsByClassName('product-name'); 
@@ -40,16 +42,21 @@ updatedPricesArray = document.getElementsByClassName('total-individual-price');
 function addTotalProductPrice(a, b) {
   return a + b;
 }
-
+ 
 function getTotalPrice() {
   getPriceByProduct();
 
   var pricesArray = [];
   for(i = 0; i < allTheProducts.length; i++) {
     var singleProductPriceValue = Number(updatedPricesArray[i].innerHTML);
+    // singleProductPriceValue = toTwoDecimals(singleProductPriceValue)
     pricesArray.push(singleProductPriceValue);
   }
 var total = pricesArray.reduce(addTotalProductPrice, 0);
+
+// make sure your total is always to 2 decimals
+total = toTwoDecimals(total);
+console.log("two decimal: ", total)
 document.getElementById('theTotal').innerHTML = total
 
 }
@@ -57,12 +64,23 @@ document.getElementById('theTotal').innerHTML = total
 
 // 1st => create name
 function createNameNode(newName){
+  // this "newName" that we pass as a parameter is just placeholder that represents 
+  // any value that user inputs through input field and that we capture through .value
+
+  // first we create "wrapper div"
   var nameDiv = document.createElement("div");
+  // we give it attribute "class" with value "col-lg-2"
   nameDiv.setAttribute("class", "col-lg-2")
+  // then we create span element
   var nameSpan = document.createElement("span");
   nameSpan.setAttribute("class", "product-name");
+  // we attach to this span whichever value user inputed, that we previously called ""newName
+  // so in order to stay consistent we keep that word
   nameSpan.innerHTML = newName;
-  nameDiv.appendChild(nameSpan)
+  // this is how we append element as a child dom element
+  // this literally means "nameSpan" is a child element of "nameDiv" element
+  nameDiv.appendChild(nameSpan);
+  // make sure all these functions have return
   return nameDiv;
 }
 
@@ -131,9 +149,21 @@ function createIndividualPrice(){
 }
 
 // 6th => create delete button
-// function createDeleteButton(){
+function createDeleteButton(){
+  var deleteButtonDiv = document.createElement('div');
+  deleteButtonDiv.setAttribute("class", "col-lg-2");
+  var deleteButton = document.createElement('button');
+  deleteButton.setAttribute("class", "btn btn-delete");
+  deleteButton.setAttribute("type", "button");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.onclick = deleteItem;
 
-// }
+  // append delete button as a child to deleteButtonDiv
+  deleteButtonDiv.appendChild(deleteButton);
+
+  // return deleteButtonDiv
+  return deleteButtonDiv;
+}
 
 // 7th => creates the whole row
 function createNewItemRow(itemName, itemPrice){
@@ -142,12 +172,15 @@ function createNewItemRow(itemName, itemPrice){
   var productPrice = createSinglePriceNode(itemPrice);
   var quantityNode = createQuantityNode();
   var indvidualTotalPrice = createIndividualPrice();
+  var deleteButtonNode = createDeleteButton();
+  console.log("deleteButtonNode: ", deleteButtonNode)
 
   // console.log("quantityNode is: ====", quantityNode)
   productRow.append(productName);
   productRow.append(productPrice);
   productRow.append(quantityNode);
   productRow.append(indvidualTotalPrice);
+  productRow.append(deleteButtonNode);
 
   // console.log("product row: ", productRow)
   return productRow;
@@ -158,13 +191,20 @@ function createNewItem(){
   var newProductName = document.getElementById('new-product-name').value;
   var newProductPrice = document.getElementById('new-product-price').value;
 
+  // call the function we created before and make sure we pass two arguments
+  // in the function above we used "itemName" and  "itemPrice" as placeholders and now we are replacing it 
+  // with realtime data, whatever user inserts in these two input fields
   var newItem = createNewItemRow(newProductName, newProductPrice);
-  // console.log("new item is: ", newItem)
+  // console.log("new item is: ", newItem);
   var newDiv = document.getElementById('create');
   // console.log("new div is: ", newDiv);
 
+  //                   what we are placing    |-------> where we are placing (insertBefore this)
+  //                               |          | 
+  //                               V          V
   newDiv.parentNode.insertBefore(newItem, newDiv)
 
+  // clear the fields after submitting
   document.getElementById('new-product-name').value = "";
   document.getElementById('new-product-price').value = "";
 }
